c6b4a45d0bf24901a675039a392087039410956d
PIPIFLOR\pipiflor <margri764@gamil.com>
2021-06-21 08:24:53 -0300

    .

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('js/sw-utils.js');
 
 const STATIC_CACHE    = 'static-v4';
-const DYNAMYC_CACHE   = 'dynamic-v1';
+const DYNAMYC_CACHE   = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -42,6 +42,10 @@ self.addEventListener('activate', e =>{
              if(key!== STATIC_CACHE && key.includes('static')){
                  return caches.delete(key)
              }
+
+             if(key!== DYNAMYC_CACHE && key.includes('dynamic')){
+                return caches.delete(key)
+            }
          }); 
      }); 
      e.waitUntil( respueta );
